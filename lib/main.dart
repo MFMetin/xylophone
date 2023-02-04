342d6f579b9cd95174fcb6c57fee92f88379d6f5
Furkan Metin <44furkan.metin@gmail.com>
2023-02-04 11:49:52 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int number) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+    assetsAudioPlayer.open(
+      Audio("assets/note$number.wav"),
+    );
+    assetsAudioPlayer.play();
+  }
+
+  Expanded buildKey(Color color, int sound) {
+    return Expanded(
+      child: TextButton(
+        child: Container(
+          color: color,
+        ),
+        onPressed: () {
+          playSound(sound);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7)
+            ],
+          ),
         ),
       ),
     );
